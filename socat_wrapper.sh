e6ca7da9156d227331b1f05c87471c5c573febae
PAN CHENXIN <pppcx97@gmail.com>
2024-04-18 23:18:17 +0800

    Update socat_wrapper.sh

--- socat_wrapper.sh
+++ socat_wrapper.sh
@@ -3,6 +3,9 @@
 #IPv4 forward
 /usr/bin/socat TCP4-LISTEN:22233,fork TCP4:191.96.242.130:62222 &
 /usr/bin/socat UDP4-LISTEN:22233,fork UDP4:191.96.242.130:62222 &
+/usr/bin/socat TCP4-LISTEN:13341,fork TCP4:191.96.242.130:13341 &
+/usr/bin/socat UDP4-LISTEN:13341,fork UDP4:191.96.242.130:13341 &
+
 
 #IPv6 forward
 #/usr/bin/socat TCP4-LISTEN:443,fork TCP6:[2606:4700:4700::1111]:80 &
