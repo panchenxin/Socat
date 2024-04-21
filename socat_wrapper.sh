4d216fcc38b241e649aca1910281dd7e21ee2ec6
PAN CHENXIN <pppcx97@gmail.com>
2024-04-21 14:05:52 +0800

    socat_wrapper.sh

--- socat_wrapper.sh
+++ socat_wrapper.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 
 #IPv4 forward
-/usr/bin/socat TCP4-LISTEN:22233,fork TCP4:191.96.242.130:62222 &
-/usr/bin/socat UDP4-LISTEN:22233,fork UDP4:191.96.242.130:62222 &
-/usr/bin/socat TCP4-LISTEN:13341,fork TCP4:191.96.242.130:13341 &
-/usr/bin/socat UDP4-LISTEN:13341,fork UDP4:191.96.242.130:13341 &
+/usr/bin/socat TCP4-LISTEN:22233,fork TCP4:45.8.186.92:55555 &
+/usr/bin/socat UDP4-LISTEN:22233,fork UDP4:45.8.186.92:55555 &
+/usr/bin/socat TCP4-LISTEN:13341,fork TCP4:191.96.242.130:44444 &
+/usr/bin/socat UDP4-LISTEN:13341,fork UDP4:191.96.242.130:44444 &
 
 
 #IPv6 forward
-#/usr/bin/socat TCP4-LISTEN:443,fork TCP6:[2606:4700:4700::1111]:80 &
-#/usr/bin/socat UDP4-LISTEN:443,fork UDP6:[2606:4700:4700::1001]:80 &
+#/usr/bin/socat TCP4-LISTEN:22233,fork TCP6:[2401:b60:5:7308:3145:cff4:300:353e]:55555 &
+#/usr/bin/socat UDP4-LISTEN:22233,fork UDP6:[2401:b60:5:7308:3145:cff4:300:353e]:55555 &
 
 wait
