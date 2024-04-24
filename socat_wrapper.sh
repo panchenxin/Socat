62b2e2949b08fd40607b70f8400f171bf01dc8cb
PAN CHENXIN <pppcx97@gmail.com>
2024-04-24 15:47:25 +0800

    socat_wrapper.sh

--- socat_wrapper.sh
+++ socat_wrapper.sh
@@ -5,6 +5,8 @@
 /usr/bin/socat UDP4-LISTEN:22233,fork UDP4:45.8.186.92:55555 &
 /usr/bin/socat TCP4-LISTEN:26941,fork TCP4:45.8.186.92:26941 &
 /usr/bin/socat UDP4-LISTEN:26941,fork UDP4:45.8.186.92:26941 &
+/usr/bin/socat TCP4-LISTEN:18365,fork TCP4:45.8.186.92:18365 &
+
 
 
 #IPv6 forward
